806d238d2e781e51bae49fdc48801a21fbaf25b4
rafly <144429611+flywzen@users.noreply.github.com>
2025-02-25 06:35:31 +0700

    Create Package.js

---
+++ .github/workflows/Package.js
@@ -0,0 +1,16 @@
+
+
+```json
+{
+  "name": "learn-js",
+  "version": "1.0.0",
+  "description": "A project to learn JavaScript",
+  "main": "index.js",
+  "scripts": {
+    "build": "webpack"
+  },
+  "dependencies": {
+    "webpack": "^5.0.0",
+    "webpack-cli": "^4.0.0"
+  }
+}
